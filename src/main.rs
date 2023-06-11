1d1f0b80890dfac180a9f65019dd89c4523a1694
Mr Ethan <119572743+Swoiksdmop@users.noreply.github.com>
2023-06-11 19:12:05 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+fn main() {
+    println!("\nWelcome to my program of converting mph to kph!!!!\n--------------------------------------------------\n\nWhat would you like to convert to kp/h from mp/h? >");
+
+    let mut mph_input = String::new();
+
+    io::stdin()
+        .read_line(&mut mph_input)
+        .expect("Failed to read line");
+
+    let mph_input: f64 = mph_input
+        .trim()
+        .parse::<f64>()
+        .expect("Failed to read line");
+
+    let formula = mph_input * 1.61;
+
+    println!("{mph_input} mp/h is {formula} kp/h");
+}
