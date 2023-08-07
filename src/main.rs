751471561d179a851521d3f717e8a226aa51ef09
Darell Kiganga <kigangadarell@gmail.com>
2023-08-07 22:20:08 +0300

    Added a repeat function and repeats for 3 times

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::io;
-fn main() {
-    println!("\nWelcome to my program of converting mph to kph!!!!\n--------------------------------------------------\n\nWhat would you like to convert to kp/h from mp/h? >");
-
+fn input() {
     let mut mph_input = String::new();
 
     io::stdin()
@@ -16,4 +14,16 @@ fn main() {
     let formula = mph_input * 1.61;
 
     println!("{mph_input} mp/h is {formula} kp/h\nThank you for using my program, and If you find issues, email me at kigangadarell@gmail.com");
+}
+
+fn repeat_input() {
+    for _ in 0..3 {
+        input();
+        println!("\n\n\nWelcome to my program of converting mph to kph!!!!\n--------------------------------------------------\n\nWhat would you like to convert to kp/h from mp/h? >");
+    }
+}
+
+fn main() {
+    println!("\n\n\nWelcome to my program of converting mph to kph!!!!\n--------------------------------------------------\n\nWhat would you like to convert to kp/h from mp/h? >");
+    repeat_input();
 }
